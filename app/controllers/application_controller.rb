60028f088949794085b46669642d9c3fb70fd141
Meghan Johnson <meghan.h.johnson@gmail.com>
2022-09-12 10:02:28 -0400

    testing font family

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def default_url_options
-    { host: ENV["www.clickandjob.live"] || "localhost:3000" }
+    { host: ENV["www.tinystorm.co"] || "localhost:3000" }
   end
 end
